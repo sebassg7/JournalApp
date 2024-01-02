c49b70e1b525efde9b7527da5909d283c60ee896
sebassg7 <sebastlopez@unicauca.edu.co>
2024-01-02 12:20:08 -0500

    Configuración de MUI con Vite

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,5 +1,11 @@
+import { AppRouter } from './router/AppRouter';
+import { AppTheme } from './theme';
+
 export const JournalApp = () => {
   return (
-    <h1>Hola Mundo !</h1>
+    <AppTheme>
+      <AppRouter/>
+    </AppTheme>
+      
   )
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { JournalApp } from './JournalApp.jsx';
+import { BrowserRouter } from 'react-router-dom';
 import './styles.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  
   <React.StrictMode>
-    <JournalApp />
+    <BrowserRouter>
+      <JournalApp />
+    </BrowserRouter>
   </React.StrictMode>,
 )
