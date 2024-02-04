b65ac910e8921b83db225954e1d658def3655639
sebassg7 <sebastlopez@unicauca.edu.co>
2024-02-03 21:26:38 -0500

    Crear una nueva nota

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,15 +2,25 @@ import { AddOutlined, MailOutline } from '@mui/icons-material';
 import { Typography, IconButton } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
 import { NothingSelectedView, NoteView } from '../views';
+import { useDispatch } from 'react-redux';
+import { starNewNote } from '../../store/journal/thunks';
 
 
 export const JournalPage = () => {
+
+  const dispatch = useDispatch();
+  
+  const onClickNewNote = () => {
+    dispatch(starNewNote());
+  };
+
   return (
     <JournalLayout>
       {/* <Typography> Metallica en Colombia 2024 Metallica en Colombia 2024 Metallica en Colombia 2024 Metallica en Colombia 2024 Metallica en Colombia 2024 Metallica en Colombia 2024 Metallica en Colombia 2024 Metallica en Colombia 2024 Metallica en Colombia 2024 Metallica en Colombia 2024 Metallica en Colombia 2024 Metallica en Colombia 2024 Metallica en Colombia 2024 Metallica en Colombia 2024 Metallica en Colombia 2024 Metallica en Colombia 2024 Metallica en Colombia 2024 Metallica en Colombia 2024 Metallica en Colombia 2024 Metallica en Colombia 2024 </Typography> */}
       <NothingSelectedView/>
       {/* <NoteView/> */}
       <IconButton
+      onClick={onClickNewNote}
       size='large'
       sx={{
         color: 'white',
--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,5 +1,11 @@
+import { collection, doc, setDoc } from 'firebase/firestore/lite';
+import { FirebaseDB } from '../../firebase/config';
+
 export const starNewNote = () => {
-    return async( dispatch ) => {
+    return async( dispatch, getState ) => {
+        
+        const { uid } = getState().auth;
+
         //uid
 
         const newNote = {
@@ -7,6 +13,10 @@ export const starNewNote = () => {
             body: '',
             date: new Date().getTime(),
         };
+
+        const newDoc = doc( collection( FirebaseDB, `${ uid }/journal/notes` ));
+        const setDocResp = await setDoc( newDoc, newNote );
+        console.log({ newDoc, setDocResp});
 
         //dispatch que debemos hacer:
         //dispatch( newNote ) => Nueva nota cuando este grabada
